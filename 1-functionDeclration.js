f8cca17d21d5f29dfd56f807fbb092b8c686005e
Persona743 <52817990+Persona743@users.noreply.github.com>
2023-05-13 23:20:04 +0330

    Delete 1-functionDeclration.js

--- 1-functionDeclration.js
+++
@@ -1,8 +0,0 @@
-//Functions in JS
-//!Q1 - What is Function Declrations?
-
-function plus(num) {
-    return num + 7;
-}
-
-//?this is a function declration | function statement | function definition
